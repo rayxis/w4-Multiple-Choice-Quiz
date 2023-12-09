534465cfb69e3064f987e67c9c378a7b31916a8d
Ray <ray@rayxis.com>
2023-12-09 09:41:40 -0600

    Finished first iteration of high score function. Finished adding questions to the quiz.

--- js/questions.js
+++ js/questions.js
@@ -12,7 +12,6 @@ const questions = [
 	{
 		"query":     "Robert Wadlow was the tallest man ever recorded. How tall was he?",
 		"answer":    "8 feet 11 inches",
-		"fact":      "At the time of his death at 22, he was 8'11\" tall. His father was 5'11\".",
 		"responses": [
 			"7 feet 9 inches",
 			"7 feet 11 inches",
@@ -23,12 +22,81 @@ const questions = [
 	{
 		"query":     "What is the biggest contributor to dust in your house?",
 		"answer":    "Dead skin cells",
-		"fact":      "Humans shed about 200 million skin cells every hour.",
 		"responses": [
 			"Pollen",
 			"Dirt",
 			"Air",
 			"Water"
+		]
+	},
+	{
+		"query":     "What is the longest, strongest bone in the human body?",
+		"answer":    "Femur",
+		"responses": [
+			"Patella",
+			"Tibia",
+			"Coccyx",
+			"Sternum"
+		]
+	},
+	{
+		"query":     "What is the national animal of Scotland?",
+		"answer":    "Unicorn",
+		"responses": [
+			"Minotaur",
+			"The Loch Ness Monster",
+			"Centaur",
+			"Phoenix"
+		]
+	},
+	{
+		"query":     "What was the profession of the man who invented cotton candy?",
+		"answer":    "Dentist",
+		"responses": [
+			"Firefighter",
+			"Inventor",
+			"Baker",
+			"Nurse"
+		]
+	},
+	{
+		"query":     "How long can some species of sharks live?",
+		"answer":    "272 - 512 years",
+		"responses": [
+			"42 - 52 years",
+			"62 - 72 years",
+			"92 - 112 years",
+			"122 - 152 years"
+		]
+	},
+	{
+		"query":     "Most of Saudi Arabia's camels are imported from which country?",
+		"answer":    "Australia",
+		"responses": [
+			"Iran",
+			"Iraq",
+			"Pakistan",
+			"India"
+		]
+	},
+	{
+		"query":     "There is a library that is half in the US and half in Canada. Where is it located on the US side?",
+		"answer":    "Derby Line, Vermont",
+		"responses": [
+			"Molson, WA",
+			"Morgan, MT",
+			"Noyes, MN",
+			"Sandy Bay Township, ME"
+		]
+	},
+	{
+		"query":     "Which organ in the human body can regenerate after up to 90% of it is removed?",
+		"answer":    "Liver",
+		"responses": [
+			"Kidney",
+			"Lungs",
+			"Pancreas",
+			"Small intestines"
 		]
 	}
 ];
--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 let quiz;
 
 class Quiz {
-	currentCount    = 0;
-	currentQuestion = null;
-	currentTimer    = 60;
-	currentTimerID  = null;
-	questions       = [];
-	score           = 0;
-	timerPenalty    = -10;
+	currentCount      = 0;
+	currentQuestion   = null;
+	currentTimerStart = 60;
+	currentTimer      = 60;
+	currentTimerID    = null;
+	highScoreLimit    = 10;
+	highScoreList     = [];
+	questions         = [];
+	score             = 0;
+	timerPenalty      = -10;
 
 	elements  = {
 		answers: null,
@@ -16,6 +19,7 @@ class Quiz {
 		timer:   document.querySelector(".timer")
 	}
 	templates = {
+		highscore: document.getElementById("template_highscore").content,
 		question:  document.getElementById("template_question").content,
 		scorecard: document.getElementById("template_scorecard").content,
 		start:     document.getElementById("template_start").content
@@ -77,16 +81,26 @@ class Quiz {
 		let cardAction;
 
 		if (cardType === "scorecard") {
-			//  Stop the timer.
-			this.stop();
-
 			//  Add the score, and an action to the restart button.
 			card.querySelector(".scorecard__text").textContent = this.score + "/" + this.questions.length;
 
 			//  Restart action.
 			cardAction = () => this.constructor(this.questions);
 			//  Start action.
-		} else if (cardType === "start") cardAction = () => this.start();
+		} else if (cardType === "start") {
+			const highScoreTree = card.querySelector(".highscores");
+
+			this.highScores();
+			this.highScoreList.forEach(highScore => {
+				let highScoreItem                                                      = this.templates.highscore.cloneNode(true);
+				highScoreItem.querySelector(".highscores__item__initials").textContent = highScore.initials;
+				highScoreItem.querySelector(".highscores__item__score").textContent    = highScore.score;
+				highScoreItem.querySelector(".highscores__item__time").textContent     = highScore.time;
+				highScoreTree.appendChild(highScoreItem);
+			});
+
+			cardAction = () => this.start();
+		}
 
 		//  Add the action to the buttons (actions have already been added to question responses).
 		if (cardType !== "question") card.querySelector(".card__button").addEventListener("click", cardAction);
@@ -96,7 +110,7 @@ class Quiz {
 	}
 
 	cardRemove() {
-		//  Remove any existing cards.
+		//  Remove the existing card, if there is one.
 		this.elements.quiz.querySelector(".card")?.remove();
 	}
 
@@ -109,7 +123,30 @@ class Quiz {
 			this.cardAdd("question");
 
 			//  No more questions, get the scorecard.
-		} else this.cardAdd("scorecard");
+		} else this.stop();
+	}
+
+	highScores(initials = null) {
+		//  If initials are passed, add them to the high scores; otherwise, get the high scores.
+		if (initials) {
+			//  Only consider adding a high score if the value is greater than 0.
+			if (this.score > 0) {
+				//  If the high score list is full, and the new score is greater than the lowest high score, drop the lowest high score.
+				if (this.highScoreList.length === this.highScoreLimit && this.highScoreList[this.highScoreList.length - 1].score < this.score)
+					this.highScoreList.pop();
+				//  Save the initials, the score and the timer value.
+				this.highScoreList.push({
+					                        initials: initials,
+					                        score:    this.score,
+					                        time:     (this.currentTimerStart - this.currentTimer)
+				                        });
+				//  Sort the high score list by score, and then by time.
+				this.highScoreList = this.highScoreList.sort(
+					(a, b) => (a.score !== b.score) ? b.score - a.score : a.time - b.time);
+				//  Save the high score list to local storage.
+				localStorage.setItem("highScores", JSON.stringify(this.highScoreList));
+			}
+		} else this.highScoreList = JSON.parse(localStorage.getItem("highScores")) || [];
 	}
 
 	shuffle(list) {
@@ -125,24 +162,35 @@ class Quiz {
 	}
 
 	start() {
-		//  Reset the score.
+		//  Reset the timer and score.
+		this.currentTimer   = this.currentTimerStart;
 		this.score          = 0;
 		//  Start the timer.
 		this.currentTimerID = setInterval(() => {
+			//  Decrement the visual timer by 1 second, and update the display field.
 			this.currentTimer--;
 			this.elements.timer.textContent = this.currentTimer + " second";
+			//  If the timer is not 1 second, add an "s" to the display field,
+			//  and if the timer is 0, go to the scorecard.
 			if (this.currentTimer !== 1) this.elements.timer.textContent += "s";
-			if (this.currentTimer === 0) this.cardAdd("scorecard");
+			if (this.currentTimer <= 0) this.stop();
 		}, 1000);
 
 		//  Start with the first question.
-		this.currentCount    = 0;
-		this.currentQuestion = this.questions[this.currentCount];
+		this.currentQuestion = this.questions[this.currentCount = 0];
 		this.cardAdd("question");
 	}
 
 	stop() {
+		//  Update timer display (in case the last question was a wrong answer).
+		this.elements.timer.textContent = this.currentTimer + " second";
+		//  If the timer is not 1 second, add an "s" to the display field,
+		//  and if the timer is 0, go to the scorecard.
+		if (this.currentTimer !== 1) this.elements.timer.textContent += "s";
+		//  Stop the timer and pull up the scorecard.
 		clearInterval(this.currentTimerID);
+		this.highScores('RB');
+		this.cardAdd("scorecard");
 	}
 }
 
@@ -150,10 +198,7 @@ quiz = new Quiz(questions);
 
 /***
  TODO:
-  Show fun fact when the card is answered.
-  Maybe add a pause button.
-  Separate Score functions?
   Definitely work on the design next (start screen and scorecard), and overall container.
-  Consider adding all of the cards in the constructor and then just toggling a hidden class/style.
-  Upon answering incorrectly (on the last question), consider reducing the timer by 10 even though it doesn't matter anymore.
+  Add initial entry method for highscores.
+  Clean up code.
  ***/
